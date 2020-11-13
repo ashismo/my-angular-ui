ff7bf7e79b5af8def5178adca71027cb09591615
Ashish Mondal <ashishkrmondal@gmail.com>
2020-11-12 23:45:34 -0500

    Debit card added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { NgMaterialMultilevelMenuModule } from 'ng-material-multilevel-menu';
 import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { DebitCardComponent } from './debit-card/debit-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CreditCardComponent,
-    MenuComponent
+    MenuComponent,
+    DebitCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/debit-card/debit-card.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Card } from '../model/card';
+import { Errors } from '../model/errors';
+
+@Component({
+  selector: 'app-debit-card',
+  templateUrl: './debit-card.component.html',
+  styleUrls: ['./debit-card.component.scss']
+})
+export class DebitCardComponent implements OnInit {
+
+  error: Errors = {
+    code: "",
+    msg: ""
+  }
+  card: Card = {
+    card: "1111-1111-1111-1111",
+    expDate: "1120",
+    cvv: "123",
+    line1: "123 main street",
+    line2: "",
+    city : "Atlanta",
+    country : "USA",
+    state : "GA",
+    zip : "30338"
+  }
+
+  errorCodes = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.errorCodes.push({code:"DD100", msg:"Something went wrong. Please Try again"});
+    this.errorCodes.push({code:"DD101", msg:"Operation failed. Please Try again"});
+    this.errorCodes.push({code:"DD102", msg:"Link failed. Please Try again"});
+    this.errorCodes.push({code:"DD103", msg:"Internal Server error. Please Try again"});
+    this.errorCodes.push({code:"DD104", msg:"Something went wrong. Please Try again"});
+  }
+
+  submit(form) {
+    console.log(form.form.controls);
+    let index = this.randomInteger(0,3);
+    this.error = this.errorCodes[index];
+  }
+
+  randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
