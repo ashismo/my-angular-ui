abb2e2e5bab2caad6f347a630f2ddbd24db4d450
Ashish Mondal <ashishkrmondal@gmail.com>
2020-11-12 21:28:51 -0500

    Added credit card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreditCardComponent } from './credit-card/credit-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardComponent
   ],
   imports: [
     BrowserModule,
