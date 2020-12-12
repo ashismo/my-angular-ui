fc7605aefea45cebeb60232bc4ad8cc725ac0db0
Ashish Mondal <ashishkrmondal@gmail.com>
2020-12-12 13:32:00 -0500

    updated file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  wordpress_server: "http://19.234.238.212/wordpress/index.php/wp-json/wp/v2",
+  wordpress_server: "http://19.234.238.248/wordpress/index.php/wp-json/wp/v2",
   lambda_api: "http://selfhel-env-1.eba-gmdmh3bp.us-east-1.elasticbeanstalk.com/content/v1/content",
   lambda_post_api: "http://selfhelpbff-env-1.eba-gmdmh3bp.us-east-1.elasticbeanstalk.com/content/v1/content/details",
   iframe_self_help: "http://localhost:4200/selfhelp?tag=",
